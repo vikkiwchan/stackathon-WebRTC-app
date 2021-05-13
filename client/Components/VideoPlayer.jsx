98bb39dede5ebb35e0435a8be8e78d70129f73c7
Vikki Chan <vikkiwchan@gmail.com>
2021-05-12 20:53:08 -0500

    added video component

--- client/Components/VideoPlayer.jsx
+++ client/Components/VideoPlayer.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Grid, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { SocketContext } from './SocketContext';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  paper: {
+    padding: '10px',
+    margin: '10px',
+  },
+}));
 
 const VideoPlayer = () => {
-  return <div>Video Player</div>;
+  const { stream, call, callAccepted, myVideo, otherVideo, name, callEnded } =
+    useContext(SocketContext);
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.gridContainer}>
+      {/* my Video */}
+      {stream && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant='h5' gutterBottom>
+              {name || 'Caller'}
+            </Typography>
+            <video
+              playsInline
+              muted
+              ref={myVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+      {/* other Video */}
+      {callAccepted && !callEnded && (
+        <Paper className={classes.paper}>
+          <Grid item xs={12} md={6}>
+            <Typography variant='h5' gutterBottom>
+              {call.name || 'Callee'}
+            </Typography>
+            <video
+              playsInline
+              ref={otherVideo}
+              autoPlay
+              className={classes.video}
+            />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
 };
 
 export default VideoPlayer;
